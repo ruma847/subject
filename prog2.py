38c9a7cfb64f74ff60727a7bc07542695d791f14
ruma847 <rumaambarish@gmail.com>
2025-04-11 21:55:11 +0530

    Add files via upload

---
+++ prog2.py
@@ -0,0 +1,12 @@
+english = 90
+computer = 100
+math = 93
+science = 93
+sum = english + computer + math + science 
+
+print("find the sum",sum)
+
+average = sum/4
+print("find the average",average)
+
+
